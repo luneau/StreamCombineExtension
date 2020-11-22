1cb3cd767dfc2121da6e34642c3ee75c0012fdda
luneau <sluneau@gmail.com>
2020-11-22 16:57:31 +0100

    fix missing files in the target

--- Package.swift
+++ Package.swift
@@ -23,7 +23,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "StreamCombineExtension",
-            dependencies: []),
+            dependencies: [],
+            path: ".",
+            sources: [
+                                "Sources/StreamCombineExtension"
+                            ]),
         .testTarget(
             name: "StreamCombineExtensionTests",
             dependencies: ["StreamCombineExtension"]),
